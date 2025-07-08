bf7c2f373bd83701b1c8060d126df55e0d3fc837
Gareth Tan <garetht@users.noreply.github.com>
2025-07-08 16:07:19 +0100

    Add changes.

--- bash_scripts/hpc_setup.sh
+++ bash_scripts/hpc_setup.sh
@@ -8,13 +8,14 @@ uv sync --no-group cuda
 
 # Installing Python dependencies that depend on the runtime GPU environment
 uv pip install bitsandbytes==0.46.1;
-uv pip install -v torch==2.7.0.dev20250310+cu128 --index-url https://download.pytorch.org/whl/cu128;
+uv pip install torch==2.7.1 --index-url https://download.pytorch.org/whl/cu128;
+
 # increasing MAX_JOBS to more than this will cause processes to be killed due to
 # likely out of memory errors, even with 200+GB of memory on the machine
-CUDA_HOME=/usr/local/cuda MAX_JOBS=24 uv pip install --force-reinstall -vv --upgrade flash-attn==2.8.0.post2 --no-build-isolation;
+CUDA_HOME=/usr/local/cuda MAX_JOBS=24 uv pip install --force-reinstall -v --upgrade flash-attn==2.8.0.post2 --no-build-isolation;
 
 source .venv/bin/activate
 
-python scripts/huggingface_downloader.py gradientai/Llama-3-8B-Instruct-262k ./downloaded-models
+python scripts/huggingface_downloader.py gradientai/Llama-3-8B-Instruct-262k ./downloaded-models/gradientai/Llama-3-8B-Instruct-262k
 
 set +e
