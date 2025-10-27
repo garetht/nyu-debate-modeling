ed9b8a7c2bb57d7ac36dbd954bb1966f6894a29c
Gareth Tan <garetht@users.noreply.github.com>
2025-10-27 17:38:37 -0400

    feat(evals): add round-one debater/judge configs

--- run_orchestrator/evals_generator/model_definitions.py
+++ run_orchestrator/evals_generator/model_definitions.py
@@ -38,64 +38,6 @@ class JudgeModelConfiguration:
     settings: ModelSettings
 
 
-ALL_VALID_DEBATERS: dict[str, DebaterModelConfiguration] = {
-    "llama-3-262k": DebaterModelConfiguration(
-        training_round=DebaterTrainingRound.UNTRAINED,
-        is_reasoning=False,
-        settings=ModelSettings(
-            model_type=ModelType.LLAMA3,
-            alias="",
-            require_quote_validation=True,
-            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/downloaded-models/gradientai/Llama-3-8B-Instruct-262k"
-        )
-    ),
-    "llama-3-262k-2025-07-31": DebaterModelConfiguration(
-        training_round=DebaterTrainingRound.SFT_ONLY,
-        is_reasoning=False,
-        settings=ModelSettings(
-            model_type=ModelType.LLAMA3,
-            alias="",
-            model_file_path="/home/ubuntu/mars-arnesen-gh/leonidtsyplenkov/models/trained_models/llama-3-mega-merged-no-judge-speeches-31.07"
-        )
-    ),
-    "llama-3-262k-41-sfted-judge": DebaterModelConfiguration(
-        training_round=DebaterTrainingRound.ROUND_TWO_DPO,
-        is_reasoning=False,
-        settings=ModelSettings(
-            model_type=ModelType.LLAMA3,
-            alias="",
-            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-3-DPO-811-FullTrainDebateRoundTwo-full-trained"
-        )
-    ),
-    "llama-3-262k-llama-sft-judge": DebaterModelConfiguration(
-        training_round=DebaterTrainingRound.ROUND_ONE_DPO,
-        is_reasoning=False,
-        settings=ModelSettings(
-            model_type=ModelType.LLAMA3,
-            alias="",
-            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-trained-for-llama-judge-finetuned-round-one"
-        )
-    ),
-    "llama-3-262k-4-turbo-judge": DebaterModelConfiguration(
-        training_round=DebaterTrainingRound.ROUND_ONE_DPO,
-        is_reasoning=False,
-        settings=ModelSettings(
-            model_type=ModelType.LLAMA3,
-            alias="",
-            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-trained-for-gpt-4-turbo-round-one"
-        )
-    ),
-    "o4-mini-rft-2025-09-15": DebaterModelConfiguration(
-        training_round=DebaterTrainingRound.RFT,
-        is_reasoning=True,
-        settings=ModelSettings(
-            model_type=ModelType.OPENAI,
-            alias="",
-            model_file_path="ft:o4-mini-2025-04-16:modulo-research-ltd:michael-and-khan-data-debater-15-09:CGO8p7XH",
-            require_quote_validation=True,
-        )
-    ),
-}
 ALL_VALID_JUDGES = {
     "gpt-4-turbo-2024-04-09": JudgeModelConfiguration(
         training_round=JudgeTrainingRound.UNTRAINED,
@@ -137,4 +79,80 @@ ALL_VALID_JUDGES = {
             model_file_path="/home/ubuntu/mars-arnesen-gh/leonidtsyplenkov/models/trained_models/llama-3-mega-merged-judge-08-10"
         )
     )
+}
+ALL_VALID_DEBATERS: dict[str, DebaterModelConfiguration] = {
+    "llama-3-262k": DebaterModelConfiguration(
+        training_round=DebaterTrainingRound.UNTRAINED,
+        is_reasoning=False,
+        settings=ModelSettings(
+            model_type=ModelType.LLAMA3,
+            alias="",
+            require_quote_validation=True,
+            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/downloaded-models/gradientai/Llama-3-8B-Instruct-262k"
+        )
+    ),
+    "llama-3-262k-2025-07-31": DebaterModelConfiguration(
+        training_round=DebaterTrainingRound.SFT_ONLY,
+        is_reasoning=False,
+        settings=ModelSettings(
+            model_type=ModelType.LLAMA3,
+            alias="",
+            model_file_path="/home/ubuntu/mars-arnesen-gh/leonidtsyplenkov/models/trained_models/llama-3-mega-merged-no-judge-speeches-31.07"
+        )
+    ),
+    "llama-3-262k-41-sfted-judge": DebaterModelConfiguration(
+        training_round=DebaterTrainingRound.ROUND_TWO_DPO,
+        is_reasoning=False,
+        settings=ModelSettings(
+            model_type=ModelType.LLAMA3,
+            alias="",
+            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-3-DPO-811-FullTrainDebateRoundTwo-full-trained"
+        )
+    ),
+    "llama-3-262k-4-turbo-judge": DebaterModelConfiguration(
+        training_round=DebaterTrainingRound.ROUND_ONE_DPO,
+        is_reasoning=False,
+        settings=ModelSettings(
+            model_type=ModelType.LLAMA3,
+            alias="",
+            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-trained-for-gpt-4-turbo-round-one"
+        )
+    ),
+    "llama-3-262k-41-judge": DebaterModelConfiguration(
+            training_round=DebaterTrainingRound.ROUND_ONE_DPO,
+            is_reasoning=False,
+            settings=ModelSettings(
+                model_type=ModelType.LLAMA3,
+                alias="",
+                model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-trained-for-gpt-41-round-one"
+            )
+    ),
+    "llama-3-262k-llama-not-sfted-judge": DebaterModelConfiguration(
+        training_round=DebaterTrainingRound.ROUND_ONE_DPO,
+        is_reasoning=False,
+        settings=ModelSettings(
+            model_type=ModelType.LLAMA3,
+            alias="",
+            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-trained-for-llama-judge-not-finetuned-round-one"
+        )
+    ),
+    "llama-3-262k-llama-sft-judge": DebaterModelConfiguration(
+        training_round=DebaterTrainingRound.ROUND_ONE_DPO,
+        is_reasoning=False,
+        settings=ModelSettings(
+            model_type=ModelType.LLAMA3,
+            alias="",
+            model_file_path="/home/ubuntu/mars-arnesen-gh/garethtan/models/trained_models/llama-trained-for-llama-judge-finetuned-round-one"
+        )
+    ),
+    "o4-mini-rft-2025-09-15": DebaterModelConfiguration(
+        training_round=DebaterTrainingRound.RFT,
+        is_reasoning=True,
+        settings=ModelSettings(
+            model_type=ModelType.OPENAI,
+            alias="",
+            model_file_path="ft:o4-mini-2025-04-16:modulo-research-ltd:michael-and-khan-data-debater-15-09:CGO8p7XH",
+            require_quote_validation=True,
+        )
+    ),
 }
